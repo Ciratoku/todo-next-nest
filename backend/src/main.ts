e3f97f7ee42435c45dac0cd140e0276bd059beff
Ciratoku <edissonman255@gmail.com>
2024-08-25 21:43:31 +0300

    d swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,15 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");
   app.enableCors();
-  const config = new DocumentBuilder()
-    .setTitle("trello api")
-    .setDescription("trello api")
-    .setVersion("1.0")
-    .addTag("default")
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
   await app.listen(3001);
 }
 bootstrap();
